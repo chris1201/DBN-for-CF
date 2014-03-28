94744fb33f6aedd9de1634cb39757730743f94a9
shuokay <shuokay@gmail.com>
2014-03-28 19:07:13 +0800

    add standard rbm

---
+++ src/srbm.cpp
@@ -0,0 +1,37 @@
+#include "srbm.h"
+#include <iomanip>
+#include <numeric>
+#include <thread>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+Srbm::Srbm(const MatrixXi& data, int nHidden, int nVisible)
+{
+    init(data, nHidden, nVisible);
+}
+
+void Srbm::init(const MatrixXi& data, int nHidden, int nVisible)
+{
+    W.setRandom(nVisible,nHidden);
+    hBias.setZero(nHidden);
+    vBias.setZero(nVisible);
+    this->nHidden=nHidden;
+    this->nVisible=nVisible;
+    //cout<<W<<hBias<<vBias;
+}
+void Srbm::expandBias(int n)
+{
+    hBias_use.setZero(n, nHidden);
+    vBias_use.setZero(n, nVisible);
+    for(int i=0; i< n; ++i)
+    {
+        hBias_use.row(i)=hBias;
+        vBias_use.row(i)=vBias;
+    }
+}
+void Srbm::train(const MatrixXi& data, float Wlr, float HBiaslr, float Vbiaslr)
+{
+    int nSample=data.rows();
+    expandBias(nSample);
+    cout<<"hBias_use\n"<<hBias_use<<'\n'<<"vBias_use\n"<<vBias_use<<endl;
+}
---
+++ src/srbm.h
@@ -0,0 +1,26 @@
+#include <Eigen/Dense>
+#include <vector>
+#include <thread>
+
+using namespace Eigen;
+using namespace std;
+typedef Matrix<bool, 1, Dynamic> RowVectorXb;
+class Srbm
+{
+public:
+    Srbm(const MatrixXi& data, int nHidden, int nVisible);
+    void init(const MatrixXi& data, int nHidden, int nVisible);
+    void train(const MatrixXi& data, float Wlr, float HBiaslr, float Vbiaslr);
+    void expandBias(int n);
+    MatrixXf W;
+    RowVectorXf hBias, vBias;
+    MatrixXf hBias_use, vBias_use;
+    int nHidden, nVisible;
+    float Wlr, hBiaslr, vbiaslr;
+    MatrixXi X,Y;
+
+private:
+    MatrixXf h0probs, hTProbs;
+    MatrixXi h0states, negData;
+    MatrixXf posProds, negProds;
+};
